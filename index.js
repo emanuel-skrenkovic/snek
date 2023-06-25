0575179fcb06d3cf50c1f6e496567cbdd9564357
Emanuel Skrenković <eskrenkovic@gmail.com>
2023-06-25 21:41:10 +0200

    very simple name input

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 const rust = import('./pkg')
 
+const name_input = (score) => {
+    let name;
+
+    window.on_name_changed = e => {
+        if (e.currentTarget.value.length > 3) return
+        name = e.currentTarget.value.toUpperCase()
+    }
+
+    window.on_submit_score = async e => {
+        e.preventDefault()
+        if (name.length < 3) return
+
+        await save_score({ name, score })
+        await on_save(score)
+    }
+
+    overlay().innerHTML = `
+    <form onsubmit="on_submit_score(event)">
+        <label>Score: ${score}</label>
+        <input id="score-input" type="text" autofocus maxlength="3" onchange="on_name_changed(event)" />
+        <button onclick="on_submit_score(event)">Save</button>\
+    </form>`
+}
+
 const save_score = (body) => fetch('/scores', {
     method: 'POST',
     body: JSON.stringify(body),
@@ -11,38 +35,38 @@ const get_scores = async () => {
     return await response.json()
 }
 
-window.game_over = async (score) => {
-    await save_score({ name: 'test', score: score })
+const on_save = async (score) => {
     const scores = await get_scores()
 
     const high_scores_list = scores.map(({ name, score }) => `
-    <tr>
-        <td>${name}</td>
-        <td>${score}</td>
-    </tr>`).reduce((agg, current) => `${agg}${current}`)
+        <tr>
+            <td>${name}</td>
+            <td>${score}</td>
+        </tr>`).reduce((agg, current) => `${agg}${current}`)
 
     const table_body = `
-    <table class="high-score-list">
-        <thead>
-        <tr>
-            <th></th>
-            <th></th>
-        </tr>
-        </thead>
-        <tbody>
-            ${high_scores_list}
-        </tbody>
-    </table>`
+        <table class="high-score-list">
+            <thead>
+            <tr>
+                <th></th>
+                <th></th>
+            </tr>
+            </thead>
+            <tbody>
+                ${high_scores_list}
+            </tbody>
+        </table>`
 
     overlay().innerHTML = `
-    You scored: ${score}
-    <br>
-    High scores:
-    <br>
-    ${table_body}`;
+        You scored: ${score}
+        <br>
+        High scores:
+        <br>
+        ${table_body}`;
 }
 
-window.pause = () => overlay().innerText = 'PAUSED'
+window.game_over    = (score) => name_input(score)
+window.pause        = () => overlay().innerText = 'PAUSED'
 window.clear_screen = () => overlay().innerText = ''
 
 window.scored = (score) => {
@@ -51,7 +75,6 @@ window.scored = (score) => {
 
 rust.then(m => {
     window.addEventListener('keypress', m.key_press_event)
-    overlay().innerHTML = `Score: 0`
     m.start()
 }).catch(console.error);
 
